c21386fb03da96429e9cd860400cd21b17a30b51
liciolentimo <lentimo@gmail.com>
2019-02-07 15:05:43 +0300

    Added tests for find,all and getId methods

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -9,28 +9,52 @@ public class ClientTest {
 
     @Test
     public void Client_instantiatesCorrectly_true() {
-        Client myClient = new Client("john", 1234, "john@test.com");
+        Client myClient = new Client("john", 1234, "john@test.com", 1);
         assertEquals(true, myClient instanceof Client);
     }
 
     @Test
     public void Client_instantiatesWithName_String() {
-        Client myClient = new Client("john", 1234, "john@test.com");
+        Client myClient = new Client("john", 1234, "john@test.com", 1);
         assertEquals("john", myClient.getClientName());
     }
 
-
     @Test
     public void Client_instantiatesWithPhone_int() {
-        Client myClient = new Client("john", 1234, "john@test.com");
+        Client myClient = new Client("john", 1234, "john@test.com", 1);
         assertEquals(1234, myClient.getClientPhone());
     }
 
-
     @Test
     public void Client_instantiatesWithEmail_String() {
-        Client myClient = new Client("john", 1234, "john@test.com");
+        Client myClient = new Client("john", 1234, "john@test.com", 1);
         assertEquals("john@test.com", myClient.getClientEmail());
+    }
+
+    @Test
+    public void getId_clientsInstantiateWithAnID() {
+        Client myClient = new Client("john", 1234, "john@test.com", 1);
+        myClient.save();
+        assertTrue(myClient.getId() > 0);
+    }
+
+    @Test
+    public void all_returnsAllInstancesOfClient_true() {
+        Client firstClient = new Client("john", 1234, "john@test.com", 1);
+        firstClient.save();
+        Client secondClient = new Client("john", 1234, "john@test.com", 1);
+        secondClient.save();
+        assertEquals(true, Client.all().get(0).equals(firstClient));
+        assertEquals(true, Client.all().get(1).equals(secondClient));
+    }
+
+    @Test
+    public void find_returnsClientWithSameId_secondClient() {
+        Client firstClient = new Client("john", 1234, "john@test.com", 1);
+        firstClient.save();
+        Client secondClient = new Client("john", 1234, "john@test.com", 1);
+        secondClient.save();
+        assertEquals(Client.find(secondClient.getId()), secondClient);
     }
 
 }
