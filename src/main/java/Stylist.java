088f28b58a682577dc5e1f814c647af7a7a3c541
liciolentimo <lentimo@gmail.com>
2019-02-07 15:02:22 +0300

    Added all(),save() and getClients() methods

--- src/main/java/Stylist.java
+++ src/main/java/Stylist.java
@@ -1,3 +1,7 @@
+import java.util.List;
+import java.util.Arrays;
+import org.sql2o.*;
+
 public class Stylist {
     private String stylistName;
     private int stylistPhone;
@@ -24,5 +28,28 @@ public class Stylist {
 
     public int getId() {
         return id;
+    }
+
+    public static List<Stylist> all() {
+        String sql = "SELECT id, stylistName,stylistPhone,stylistEmail FROM stylists";
+        try (Connection con = DB.sql2o.open()) {
+            return con.createQuery(sql).executeAndFetch(Stylist.class);
+        }
+    }
+
+    public void save() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "INSERT INTO stylists (stylistName, stylistPhone, stylistEmail) VALUES (:stylistName, :stylistPhone, :stylistEmail)";
+            this.id = (int) con.createQuery(sql, true).addParameter("stylistName", this.stylistName)
+                    .addParameter("stylistPhone", this.stylistPhone).addParameter("stylistEmail", this.stylistEmail)
+                    .executeUpdate().getKey();
+        }
+    }
+
+    public List<Client> getClients() {
+        try (Connection con = DB.sql2o.open()) {
+            String sql = "SELECT * FROM clients where stylistId=:id";
+            return con.createQuery(sql).addParameter("id", this.id).executeAndFetch(Client.class);
+        }
     }
 }
