f344ae188f07588a92b4015381499eef37a0900e
liciolentimo <lentimo@gmail.com>
2019-02-21 14:55:46 +0300

    Added mailchimp

--- src/main/java/DB.java
+++ src/main/java/DB.java
@@ -27,3 +27,10 @@ sql2o = new Sql2o("jdbc:postgresql://" + host + ":" + port + path, username, pas
       }
   }
 }
+
+
+// import org.sql2o.*;
+
+// public class DB {
+//  	public static Sql2o sql2o = new Sql2o("jdbc:postgresql://localhost:5432/hair_salon", "licio", "9353");
+// }
